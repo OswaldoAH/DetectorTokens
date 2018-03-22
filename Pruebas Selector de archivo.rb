0e5b6fd47ed4f12279b6a4efa70eaa437af1bb1e
Oswaldo Alvarez <mynor4755@hotmail.com>
2018-03-22 02:17:48 -0600

    Modifique la clase Prueba Selector para que obtenga nada mas la ruta Y jale algunos ejemplos del manual xd

--- Pruebas Selector de archivo.rb
+++ Pruebas Selector de archivo.rb
@@ -1,10 +1,7 @@
-Shoes.app(:title => "Boton con selector de archivo") do
-	a
-    button "Abrir Archivo" do
-        filename = ask_open_file
-        a= File.read(filename)
-        
-    end
-    para a
-    
-end
+Shoes.app(:title => "Boton con selector de archivo",:width => 400, :height => 450, :resizable => true) do
+	background rgb(112,128,144), :height => 50, :left => 10
+	button "Abrir Archivo", :top =>20, :left => 20 do
+		sel = ask_open_file title: "Seleccione un archivo"
+		para "#{sel}", :top=>300 
+   end
+ end
