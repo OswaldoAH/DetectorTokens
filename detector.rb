e1ae7e1ac21dbda30fa7c58deac8915fa6701afa
hectortllo <hectortllo.@gmail.com>
2018-04-05 19:35:41 -0600

    Hice más pequeña la pantalla, agregué un list_box, moví un poco los elementos y agregué un botón para mostrar las gráficas.

--- detector.rb
+++ detector.rb
@@ -573,8 +573,8 @@ class Ventana
 
 	def disenio
 		Shoes.app :title=>"Detector de Tokens",
-			:width=>1100, 
-			:height=>525,
+			:width=>750, 
+			:height=>500,
 		 	:margin=>10,
 		 	:resizable => false do
 		 	@detector = DetectorTokens.new
@@ -589,34 +589,44 @@ class Ventana
 				@titulo.family = "Comic Sans"
 			end
 
+			@arrayNuevo = Array.new
 			#Divido la pantalla a la mitad
 			stack :width=>"50%" do
 				#Uso el 100% de la mitad que partí
 				flow :width=>"100%" do
 
 					#Caption para mostrar un texto debajo de una imagen
-					@captionSeleccionarArchivo = caption(:top=>320, :left => 150)
+					@captionSeleccionarArchivo = caption(:top=>320, :left => 125)
 					@captionSeleccionarArchivo.text = "Seleccionar Archivo"
 					@captionSeleccionarArchivo.size = 12
 					
 					#Caption para mostrar un texto debajo de una imagen
-					@captionPlay = caption(:top=>320, :left=>340)
+					@captionPlay = caption(:top=>320, :left=>320)
 					@captionPlay.text = "Ejecutar"
 					@captionPlay.size = 10
 
 					#Imagen para seleccionar el archivo al que se le van a encontrar los tokens
-					@imgSelect = image("seleccionar.png", :top=>140, :left =>170, :width=>100, :height=>100)
+					@imgSelect = image("seleccionar.png", :top=>140, :left =>145, :width=>100, :height=>100)
 					@imgSelect.click do
 						@filename = ask_open_file
 						@filename = @filename.gsub("\\", "/")
 						@captionPath.text = @filename
 						@detector.rutaArchivo(@filename)
-						#Por ahora se muestra en el edit_box lo que está en el archivo, pero es solo de prueba. En ese cuadro se van a mostrar
-						#los errores que tenga el txt o los tokens que se hallan encontrado
+					end
+
+					#ComboBox
+					list_box :top=>70, :left =>500, :items => ["Palabras Reservadas", "Variables Int"],
+						:choose => "Palabras Reservadas" do |list|
+							list_box = list.text
+						end
+
+					#Botón mostrar gráfica
+					button "Mostrar Gráfica", :top=>130, :left =>515 do
+						grafica1(@values1, @x_axis1)
 					end
 
 					#Imagen para ejecutar el detector de tokens
-					@imgPlay = image("play.png", :top=>140, :left => 320, :width => 100, :height=>100)
+					@imgPlay = image("play.png", :top=>140, :left => 295, :width => 100, :height=>100)
 					@imgPlay.click do
 						@var = @captionPath.text.length
 						#Condición para verificar si ya se seleccionó un archivo o no
@@ -630,39 +640,41 @@ class Ventana
 							end
 							#
 							alert string, :title => "Palabras Reservadas"
-							grafica1
+							#grafica1
 						else
 							alert("Seleccione un archivo")
 						end
 					end
 
 					#Caption que se usa sólo para colocar un texto
-					@captionRuta = caption(:top=>170, :left=>60)
+					@captionRuta = caption(:top=>170, :left=>30)
 					@captionRuta.text = "Ruta: "
 					#Caption en el que se coloca la ruta del archivo seleccionado
-					@captionPath = caption(:top =>170, :left=>120)
-					@captionPath.size = 13
+					@captionPath = caption(:top =>173, :left=>80)
+					@captionPath.size = 12
 					@captionPath.width = 400
 				end
 			end
 
 			#Se usa la otra mitad de la pantalla que se dividió arriba
 			#método para imprimir la gráfica 1
-			def grafica1
-				stack :width => "50%" do
-					flow :width => "100%" do
-						widget_width = 500
-						widget_height = 400
-				
-						@grf = plot widget_width, widget_height, title: "Gráfica", caption:
-						"Amazing!!", font: "Mono", auto_grid: false,
-						default: "skip", background: cornsilk, chart: "pie", boundary_box: false
-					end
-				end
 
-				@grf.add values: @values1, labels: @x_axis1,
-					name: "", min: 0, max: 30, color: rgb(220, 20, 60, 172),
-					points: true, strokewidth: 25
+
+			def grafica1(valores, eje)
+				Shoes.app width: 500, height: 500 do
+					widget_width = 500
+					widget_height = 500
+					@arrayNuevo = valores
+					@arrayNuevo.sort
+					
+					@grf = plot widget_width, widget_height, title: "Gráfica", caption:
+					"Amazing!!", font: "Mono", auto_grid: true,
+					default: "skip", background: cornsilk, chart: "column", boundary_box: true
+					
+					@grf.add values: valores, labels: eje,
+						name: "", min: 0, max: @arrayNuevo[0]+1, color: rgb(220, 20, 60, 172),
+						points: true, strokewidth: 25
+				end
 			end
 		end
 	end
